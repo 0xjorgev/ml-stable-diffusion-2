94daefa63337ba2d0a043a6d00dd56d544f09b6b
Jorge Mendoza <jorgevmendoza@gmail.com>
2023-01-18 17:12:12 +0100

    smaller image size for testing

--- swift/StableDiffusion/pipeline/SafetyChecker.swift
+++ swift/StableDiffusion/pipeline/SafetyChecker.swift
@@ -65,7 +65,7 @@ public struct SafetyChecker: ResourceManaging {
 
 //        let width = inputShape[2].intValue
 //        let height = inputShape[3].intValue
-        let width = 64
+        let width = 128
         let height = 64
 
         let resizedImage = try resizeToRGBA(image, width: width, height: height)
@@ -81,7 +81,7 @@ public struct SafetyChecker: ResourceManaging {
                 // No adjustment, use default threshold
                 adjustmentName : MLMultiArray(MLShapedArray<Float32>(scalars: [0], shape: [1])),
                 // Supplying dummy images to be filtered (will be ignored)
-                imagesNames    : MLMultiArray(shape:[1, 64, 64, 3], dataType: .float16)
+                imagesNames    : MLMultiArray(shape:[1, 128, 64, 3], dataType: .float16)
             ]
         ) else {
             throw SafetyCheckError.modelInputFailure
